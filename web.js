c9b7aa34e5f358ac254a2595cffd2d3ac99f3291
Felippe Nardi <felippe@nardi.me>
2013-02-17 19:36:31 +0000

    testing redis

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
-
-
-redis.set('foo','miga');
-
-redis.get('foo', function(err, value) {
-    console.log('foo is: ' + value);
-}
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     response.send('Hello World!');
+});
+
+redis.set('foo', 'bar');
+
+redis.get('foo', function(err, value) {
+    console.log('foo is: ' + value);
 });
 
 var port = process.env.PORT || 5000;
