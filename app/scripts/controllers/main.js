f4fa27ef9e9b115b94c7bd66d980188c991596e0
Felippe Nardi <felippe@nardi.me>
2013-03-03 19:45:28 +0000

    Preparing dummy json to provide image urls

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,10 +7,29 @@ angular.module('herokuApp')
       'AngularJS',
       'Testacular'
     ];
-    $scope.awesomeCats = {
-        "gato": {
+    $scope.gallery = {
+        "group": {
             "name": "Felix";
-            "images": ["url1", "url2", "url3"];
+            "images": ["http://placekitten.com/2000/2000"];
+        },
+        "group": {
+            "name": "Sortudo";
+            "images": ["http://placekitten.com/2001/2000"];
+        },
+        "group": {
+            "name": "Soft Kitty";
+            "images": [
+                "http://placekitten.com/2001/2000",
+                "http://placekitten.com/1001/1000",
+                "http://placekitten.com/1001/1005",
+                "http://placekitten.com/1011/1005",
+                "http://placekitten.com/1150/1055",
+                "http://placekitten.com/1150/1080"
+            ];
+        },
+        "group": {
+            "name": "Camisa";
+            "images": ["http://skreened.com/render-product/c/u/i/cuiccqawsyevvvcgqeoo/you-ve-cat-to-be-kitten-me-right-meow-v-neck.american-apparel-unisex-v-neck-tee.athletic-grey.w760h760.jpg"];
         }
     }
   });
