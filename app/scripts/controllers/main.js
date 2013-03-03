b632f1ed674fc956f3a19577a285791314e487e7
Felippe Nardi <felippe@nardi.me>
2013-03-03 22:05:11 +0000

    Fixing controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,15 +9,15 @@ angular.module('herokuApp')
     ];
     $scope.gallery = {
         "group": {
-            "name": "Felix";
-            "images": ["http://skreened.com/render-product/c/u/i/cuiccqawsyevvvcgqeoo/you-ve-cat-to-be-kitten-me-right-meow-v-neck.american-apparel-unisex-v-neck-tee.athletic-grey.w760h760.jpg"];
+            "name": "Felix",
+            "images": ["http://skreened.com/render-product/c/u/i/cuiccqawsyevvvcgqeoo/you-ve-cat-to-be-kitten-me-right-meow-v-neck.american-apparel-unisex-v-neck-tee.athletic-grey.w760h760.jpg"]
         },
         "group": {
-            "name": "Sortudo";
-            "images": ["http://placekitten.com/2001/2000"];
+            "name": "Sortudo",
+            "images": ["http://placekitten.com/2001/2000"]
         },
         "group": {
-            "name": "Soft Kitty";
+            "name": "Soft Kitty",
             "images": [
                 "http://placekitten.com/2001/2000",
                 "http://placekitten.com/1001/1000",
@@ -25,7 +25,7 @@ angular.module('herokuApp')
                 "http://placekitten.com/1011/1005",
                 "http://placekitten.com/1150/1055",
                 "http://placekitten.com/1150/1080"
-            ];
+            ]
         }
-    }
+    };
   });
